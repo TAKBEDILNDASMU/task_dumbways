caee5e0a9fa94fb7c34350bf97eb77bb8ecec735
Titan Apriliyan <titanaprilian73@gmail.com>
2023-10-05 19:52:23 +0700

    day-06 responsive styling

---
+++ assets/js/button.js
@@ -0,0 +1,7 @@
+let isOpen = false // true
+function openhumberger() {
+  let humbergerNav = document.getElementById("humberger-nav-container")
+  let testing = document.getElementById("humberger-nav-container")
+
+  humbergerNav.classList.toggle("active")
+}
